84cb39c0e8acc9c128f5769c09ab0c8f5e9cfc4e
Imanol Conde <imanolcondeimanol@gmail.com>
2022-05-17 17:25:35 +0200

    Muchisimos cambios en la app

--- index.js
+++ index.js
@@ -1,9 +1,81 @@
 const express = require("express");
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
-
 const app = express();
 const http = require('http');
+const _ = require('lodash');
 
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST || 'localhost';
+
+const path = require('path');
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + '/public')))
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la web');
+});
+
+function saveFile(file, data) {
+    //move photo to uploads directory
+    file.mv('./uploads/' + file.name);
+
+    //push file details
+    data.push({
+        name: file.name,
+        mimetype: file.mimetype,
+        size: file.size
+    });
+}
+
+app.post('/uploads', async (req, res) => {
+
+    console.log('Upload');
+
+    try {
+        if (!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            let data = [];
+
+            if (req.files.files.constructor !== Array) saveFile(req.files.files, data);
+            else {
+                //loop all files
+                _.forEach(_.keysIn(req.files.files), (key) => {
+                    let file = req.files.files[key];
+
+                    saveFile(file, data);
+                });
+            }
+            //return response
+            res.send({
+                status: true,
+                message: 'Files are uploaded',
+                data: data
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+
+});
+
+
+//Sockets para mensajes de chat
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
@@ -12,21 +84,10 @@ io.on('connection', socket => {
 
     socket.on('chat', message => {
         console.log('From client: ', message);
-        console.log('With socket: ', socket);
         io.emit('chat', message);
     })
 })
 
-const port = process.env.PORT || 3000;
-const hostname = process.env.HOST || 'localhost';
-
-const path = require('path')
-app.use(express.static(path.join(__dirname + '/public')))
-
-app.get('/', (req, res) => {
-    res.send('Bienvenido a la web');
-});
-
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log(`Servidor corriendo en puerto ${port} y host ${hostname}`);
 })
--- public/script.js
+++ public/script.js
@@ -1,19 +1,4 @@
-const socket = io()
-
-const chat = document.querySelector('.chat-form')
-const Input = document.querySelector('.chat-input')
-
-chat.addEventListener('submit', event => {
-  event.preventDefault();
-  if(Input.value) {
-    playMessageSent();
-    socket.emit('chat', Input.value || getFakeMessage());
-    Input.value = '';
-  }
-  return false;
-});
-
-const chatWindow = document.querySelector('.chat-window');
+const socket = io();
 
 const getDate = () => {
   var today = new Date();
@@ -50,46 +35,177 @@ function getFakeMessage() {
   mollitia nostrum repellat distinctio?`;
 }
 
-const renderMessage = message => {
+const renderMessage = (text, files) => {
 
-  let div = document.createElement('div'); 
+  const chatWindow = document.querySelector('.chat-window');
+  let div = document.createElement('div');
 
-  div.innerHTML = `<i id="user-icon" class="icon fa-solid fa-user"></i>
-  <span class="name">${getUser()}</span>
-  <span class="date">${getDate()}</span>
-  <span class="text"
-    >${message}</span>`;
+  let template = `
+    <i id="user-icon" class="icon fa-solid fa-user"></i>
+    <span class="name">${getUser()}</span>
+    <span class="date">${getDate()}</span>
+    <span class="text">${text}</span>`;
+    
+  template += renderMessageFiles(files);
+  div.innerHTML = template;
   div.classList.add('user-message');
-  
+
 
   chatWindow.appendChild(div);
   scrollMessages();
+
 }
 
-socket.on('chat', message => {
-  console.log('From server: ', message);
-  playMessageSent();
-  renderMessage(message);
-});
-
-let audio = document.getElementById('message-sent-audio');
+function renderMessageFiles(files) {
+  let template = '';
+  if (files != null && typeof files != 'undefined') {
+    template += '<div class="files">';
+    for (let key in Object.keys(files)) {
+      let file = files[key];
+      template += `
+        <div class="file">
+          <i class="represent icon fa-solid fa-file"></i>
+          <div class="info">
+            <a href="#" class="name">${file.name}</a>
+            <span class="size">${file.size}</span>
+          </div>
+          <i class="download icon fa-solid fa-download"></i>
+        </div>`;
+    };
+    template += '</div>';
+  }
+  return template;
+}
 
 function playMessageSent() {
-  
+  let audio = document.getElementById('message-sent-audio');
   audio.play();
+}
+
+function refreshAudio(audioSrc) {
+  let audio = document.getElementById('message-sent-audio');
+  audio.setAttribute('src', audioSrc);
 }
 
 function scrollMessages() {
   var l = document.getElementsByClassName("user-message").length;
-  document.getElementsByClassName("user-message")[l-1].scrollIntoView();
+  document.getElementsByClassName("user-message")[l - 1].scrollIntoView();
 }
 
-let file = document.getElementById('chat-file');
+function renderFile(file) {
 
-file.addEventListener('change', (event) => {
-  console.log(file.value, event);
-}); 
+  let div = document.createElement('div');
+  let filePrevs = document.getElementsByClassName('file-prevs')[0];
 
-function refreshAudio(audioSrc) {
-  audio.setAttribute('src', audioSrc);
+  div.innerHTML = `<i class="icon fa-solid fa-file"></i>
+    <p class="file-name">
+      ${file.name}
+    </p>`;
+  div.classList.add('file-prev');
+
+  filePrevs.appendChild(div);
 }
+
+function changeFormWrap(forFiles) {
+
+  //Si queremos poner archivos habilitamos la previsualización
+  if (forFiles) {
+    let formWrap = document.getElementsByClassName('form-wrap')[0];
+    formWrap.classList.replace('without-files', 'with-files');
+    let filePrevs = document.getElementsByClassName('file-prevs')[0];
+    filePrevs.style.display = 'flex';
+  //Si queremos borrar archivos de la previsualización deshabilitamos 
+  } else {
+    let formWrap = document.getElementsByClassName('form-wrap')[0];
+    formWrap.classList.replace('with-files', 'without-files');
+    let filePrevs = document.getElementsByClassName('file-prevs')[0];
+    filePrevs.style.display = 'none';
+  }
+
+}
+
+function renderFileChange(file) {
+
+  let filePrevs = document.getElementsByClassName('file-prevs')[0];
+  let thereAreFilesToRender = file.files.length > 0;
+  changeFormWrap(thereAreFilesToRender);
+  filePrevs.innerHTML = '';
+
+  if(!thereAreFilesToRender) return;
+  
+  for (let key in Object.keys(file.files)) {
+    renderFile(file.files[key]);
+  };
+}
+
+function resetFilePrev() {
+  changeFormWrap(false);
+  let filePrevs = document.getElementsByClassName('file-prevs')[0];
+  filePrevs.innerHTML = '';
+}
+
+function sendFormData(text) {
+  var form = $('.chat-form')[0];
+  var data = new FormData(form);
+
+  $.ajax({
+    type: "POST",
+    enctype: 'multipart/form-data',
+    url: "/uploads",
+    data: data,
+    processData: false,
+    contentType: false,
+    cache: false,
+    timeout: 600000,
+    success: function (data) {
+
+      console.log("SUCCESS : ", data);
+      socket.emit('chat', {text: text, files: data.data});
+
+    },
+    error: function (e) {
+
+      console.log("ERROR : ", e);
+
+    }
+  });
+}
+
+function main() {
+
+  const chat = document.querySelector('.chat-form');
+  const Input = document.querySelector('.chat-input');
+  const chatWindow = document.querySelector('.chat-window');
+  let audio = document.getElementById('message-sent-audio');
+  let file = document.getElementById('chat-file');
+  let filePrevs = document.getElementsByClassName('file-prevs')[0];
+
+  chat.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log(file.files, event);
+    if (Input.value || file.files.length > 0) {
+      sendFormData(Input.value);
+      Input.value = '';
+      resetFilePrev();
+      $('.chat-form')[0].reset();
+      playMessageSent();
+    }
+    return false;
+  });
+
+
+  socket.on('chat', message => {
+    console.log('From server: ', message);
+    playMessageSent();
+    renderMessage(message.text, message.files);
+  });
+
+  file.addEventListener('change', (event) => {
+
+    renderFileChange(file);
+    Input.focus();
+
+  });
+}
+
+$(document).ready(main);
