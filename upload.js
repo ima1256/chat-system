68f5c229e72ddadaad848af7bc671fbcc30c286d
Imanol Conde <imanolcondeimanol@gmail.com>
2022-05-17 17:25:54 +0200

    Muchisimos cambios en la app

---
+++ upload.js
@@ -0,0 +1,9 @@
+let http = require('http');
+const path = require('path');
+require('dotenv').config();
+let formidable = require('formidable');
+
+http.createServer(function (req, res) {
+
+    
+}).listen(5000, 'localhost');
